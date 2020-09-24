3f736dc5d4572af79ec927bc124773c49650121b
icyftl <savap0@yandex.ru>
2020-09-25 01:39:44 +0300

    Sh fix 1

--- start.sh
+++ start.sh
@@ -9,5 +9,8 @@ sudo docker cp ezgen_api:/opt/app/ezgen.db ./api
 sudo docker cp ezgen_api:/opt/app/tmp/ ./tmp
 sudo docker cp ezgen_telegram:/opt/app/telegram.db ./telegram
 
+touch api/ezgen.db
+touch telegram/telegram.db
+
 docker-compose down
 docker-compose up --build --force-recreate -d
