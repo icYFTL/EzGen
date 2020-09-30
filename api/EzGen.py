b0c277bc4f62c44b8316e30e15c5b527dd3c399c
icyftl <savap0@yandex.ru>
2020-09-30 04:16:33 +0300

    Flag fix

--- api/EzGen.py
+++ api/EzGen.py
@@ -1,4 +1,4 @@
 from source.api.api import *
 from core import config
 
-app.run(host=config['host'], port=config['port'], debug=True)
+app.run(host=config['host'], port=config['port'], debug=False)
